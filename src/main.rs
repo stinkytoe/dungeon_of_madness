d30797ad445f68d1619c3a994266552b9dde957f
stinkytoe <stinkytoe42@yahoo.com>
2026-01-24 21:32:41 -0600

    refine parse_level_code

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,24 @@ const WALL_RIGHT: u16 = 0x2;
 const WALL_DOWN: u16 = 0x4;
 const WALL_LEFT: u16 = 0x8;
 
+/// We track the level under the player in a resourse.
+///
+/// Used by [track_current_level]
 #[derive(Resource, Deref, DerefMut)]
 struct CurrentLevel(Entity);
 
+/// When we move to another level, or when we start, we send this event for all
+/// chunks in the directions north, east, south, and west.
+///
+/// This is handled by the [attempt_spawn_level] observer
 #[derive(Event, Deref)]
 struct AttemptSpawnLevel(Vec2);
 
+/// The current state. This is a very simple state. We stay in
+/// [GameState::Loading] until we find the start hall has been loaded.
+/// Once loaded, [wait_for_start_hall] will send [AttemptSpawnLevel] messages
+/// in the four cardinal directions and transition to [GameState::Playing],
+/// where we remain until the player closes the window.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, Hash, States)]
 enum GameState {
     #[default]
@@ -49,6 +61,10 @@ enum GameState {
     Playing,
 }
 
+/// A custom 2d materal for drawing the clouds layer overlay.
+///
+/// Normally you wouldn't parameterize so many things, I just wanted to allow
+/// people to play with the parameters in bevy_inspector_egui.
 #[derive(Asset, AsBindGroup, Debug, Clone, Reflect)]
 struct CloudsMaterial {
     #[uniform(0)]
@@ -60,15 +76,21 @@ struct CloudsMaterial {
 impl Default for CloudsMaterial {
     fn default() -> Self {
         Self {
-            params0: Vec4::new(0.015, 2.0, 15.0, 0.0),
-            params1: Vec4::new(0.5, 1.5, -0.25, 0.25),
+            params0: Vec4::new(
+                0.015, // Overall speed.
+                2.0, 15.0, // Overall scale.
+                0.0,  // The timer. Mutated in [cloud_material_update_time].
+            ),
+            params1: Vec4::new(
+                0.5, // The speed multiplier for the left scrolling of the clouds.
+                1.5, -0.25, // The linear transform we apply to the color.
+                0.25,  // The alpha of the layer.
+            ),
         }
     }
 }
 
-#[derive(Component)]
-struct Clouds;
-
+/// Implement [CloudsMaterial] as a [Material2d] with Bevy.
 impl Material2d for CloudsMaterial {
     fn fragment_shader() -> ShaderRef {
         CLOUDS_SHADER_PATH.into()
@@ -79,20 +101,48 @@ impl Material2d for CloudsMaterial {
     }
 }
 
+/// Marker component for the clouds layer.
+#[derive(Component)]
+struct Clouds;
+
+/// Convenience function for getting the `code` for the given level name.
+///
+/// Expected format:
+/// - If the level name is `"Start_Hall"`, then return zero.
+/// - Otherwise, we expect the level name to be in the form of `"Level_n"` where
+///   _n_ is between 0 to 15.
+///     
 fn parse_level_code(level_name: &str) -> u16 {
-    if level_name == "Start_Hall" {
-        0
-    } else {
-        level_name[6..].parse().expect("bad level name?")
+    match level_name {
+        "Start_Hall" => 0,
+        name if &name[0..6] == "Level_" => {
+            let code = level_name[6..]
+                .parse()
+                .expect("Couldn't parse code: {rest}");
+            if (0..=15).contains(&code) {
+                code
+            } else {
+                panic!("Code out of range! {code}")
+            }
+        }
+
+        bad_name => panic!("bad level name! {bad_name}"),
     }
 }
 
+/// Spawn all the initial components:
+/// - Camera
+/// - Start Hall
+/// - Text Label
+/// - Clouds mesh
 fn setup(
     asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<CloudsMaterial>>,
     mut commands: Commands,
 ) {
+    // The camera, initialized to the default scale. It's actually not where we
+    // want, but when the skeleton spawns later on then it'll get moved.
     commands.spawn((
         Camera2d,
         Transform::default().with_scale(Vec2::splat(CAMERA_ZOOM_DEFAULT).extend(1.0)),
@@ -128,12 +178,10 @@ fn setup(
         },
     ));
 
-    // The foreground mesh.
+    // The clouds mesh.
     commands.spawn((
         Name::new("Clouds"),
         Mesh2d(meshes.add(Rectangle::from_corners(
-            // -WINDOW_RESOLUTION.as_vec2() / 2.0,
-            // WINDOW_RESOLUTION.as_vec2() / 2.0,
             Vec2::splat(-9999.0),
             Vec2::splat(9999.0),
         ))),
@@ -143,6 +191,7 @@ fn setup(
     ));
 }
 
+/// Update the mesh with the current game time.
 fn cloud_material_update_time(
     time: Res<Time>,
     material: Single<&MeshMaterial2d<CloudsMaterial>>,
@@ -152,8 +201,14 @@ fn cloud_material_update_time(
     material.params0.w = time.elapsed_secs();
 }
 
+/// Move the camera and clouds mesh to always be centered over the player
+/// skeleton.
+///
+/// This system won't run until the skeleton AND the cloud mesh are
+/// fully loaded. This is fine for this demo, but a more serious game should
+/// probably consider a more robust solution.
 #[allow(clippy::type_complexity)]
-fn camera_follow_skeleton(
+fn camera_and_clouds_follow_skeleton(
     skeleton_transform: SingleByIid<
         SKELETON_IID,
         &Transform,
@@ -176,7 +231,8 @@ fn camera_follow_skeleton(
     cloud_transform.translation = skeleton_location.with_z(CLOUDS_Z);
 }
 
-fn camera_zoom_commands(
+/// Update the camera scale in response to the mouse wheel events.
+fn camera_mouse_wheel_zoom(
     time: Res<Time>,
     mut camera: Single<&mut Transform, With<Camera2d>>,
     mut mouse_scroll: MessageReader<MouseWheel>,
@@ -188,6 +244,14 @@ fn camera_zoom_commands(
     }
 }
 
+/// This system will only run once. When the start hall is loaded and the
+/// [ShieldtankWorldBounds] component is added:
+/// - Create the [CurrentLevel] resource
+/// - Send four [AttemptSpawnLevel] in each direction
+/// - Change state to [GameState::Playing].
+///
+/// [attempt_spawn_level] will spawn levels in each direction, since there won't
+/// be any there already.
 fn wait_for_start_hall(
     level_query: SingleByIid<START_HALL_IID, Entity, With<ShieldtankWorldBounds>>,
     mut next_state: ResMut<NextState<GameState>>,
@@ -203,6 +267,12 @@ fn wait_for_start_hall(
     next_state.set(GameState::Playing);
 }
 
+/// Watch the skeleton location, and update the [CurrentLevel] resource if
+/// changed.
+///
+/// Also, if the level changed, we send a [AttemptSpawnLevel] signal in each
+/// of the four surrouding directions. [attempt_spawn_level] will spawn a level
+/// there if the space is open.
 fn track_current_level(
     skeleton_location: SingleByIid<
         SKELETON_IID,
@@ -251,6 +321,7 @@ fn track_current_level(
     }
 }
 
+/// The observer which responds to the [AttemptSpawnLevel] event.
 fn attempt_spawn_level(
     attemt_level_location: On<AttemptSpawnLevel>,
     level_query: QueryByGlobalBounds<&Name, With<ShieldtankLevel>>,
@@ -258,11 +329,14 @@ fn attempt_spawn_level(
     mut rand: Local<StdRand>,
     mut commands: Commands,
 ) {
+    // Coerce the event into the global `Vec2`.
     let attempt_level_location: Vec2 = **attemt_level_location;
     info!("Spawning new level at: {attempt_level_location}");
 
     const CENTER_OFFSET: Vec2 = Vec2::new(LEVEL_SIZE / 2.0, -LEVEL_SIZE / 2.0);
 
+    // QueryByGlobalBounds is inclusive for all bounds, so we sample from the
+    // center instead of attempt_level_location, which is the upper left corner.
     if level_query
         .single_by_location(attempt_level_location + CENTER_OFFSET)
         .is_ok()
@@ -270,26 +344,32 @@ fn attempt_spawn_level(
         return;
     }
 
+    // `Some(code)` if there's a level to the north, `None` if there's no
+    // level there (yet).
     let level_up_code = level_query
         .single_by_location(attempt_level_location + CENTER_OFFSET + LEVEL_UP)
         .ok()
         .map(|level_name| parse_level_code(level_name));
 
+    // Similar, but to the east.
     let level_right_code = level_query
         .single_by_location(attempt_level_location + CENTER_OFFSET + LEVEL_RIGHT)
         .ok()
         .map(|level_name| parse_level_code(level_name));
 
+    // Similar, but to the south.
     let level_down_code = level_query
         .single_by_location(attempt_level_location + CENTER_OFFSET + LEVEL_DOWN)
         .ok()
         .map(|level_name| parse_level_code(level_name));
 
+    // Similar, but to the west.
     let level_left_code = level_query
         .single_by_location(attempt_level_location + CENTER_OFFSET + LEVEL_LEFT)
         .ok()
         .map(|level_name| parse_level_code(level_name));
 
+    // Generate a new number from 0 to 15
     let mut rand = rand.next_lim_u16(15);
 
     let mut fix_rand_by_code = |code: Option<u16>, wall: u16, opposite_wall: u16| {
@@ -302,13 +382,15 @@ fn attempt_spawn_level(
         }
     };
 
+    // If a level already exists in the given direction, explicitly set the
+    // respective bit, so the doors match.
     fix_rand_by_code(level_up_code, WALL_UP, WALL_DOWN);
     fix_rand_by_code(level_right_code, WALL_RIGHT, WALL_LEFT);
     fix_rand_by_code(level_down_code, WALL_DOWN, WALL_UP);
     fix_rand_by_code(level_left_code, WALL_LEFT, WALL_RIGHT);
 
+    // Spawn the new level, using the bevy_ldtk_asset asset path.
     let new_level_asset_label = format!("{PROJECT_FILE}#world:Dungeon/Level_{rand}");
-
     commands.spawn((
         ShieldtankLevel {
             handle: asset_server.load(new_level_asset_label),
@@ -355,14 +437,14 @@ fn player_keyboard_commands(
 
     let dir = (up_pressed, right_pressed, down_pressed, left_pressed);
 
-    // Do we need to flip the sprite?
+    // Flip the sprite based on the direction of movement.
     match dir {
         KEY_UP_RIGHT | KEY_RIGHT | KEY_DOWN_RIGHT => tile.flip_x(false),
         KEY_UP_LEFT | KEY_LEFT | KEY_DOWN_LEFT => tile.flip_x(true),
         _ => {}
     };
 
-    // Construct a direction vector
+    // Construct a direction normal.
     let dir = match dir {
         KEY_UP => Vec2::new(0.0, 1.0),
         KEY_UP_RIGHT => Vec2::new(FRAC_1_SQRT_2, FRAC_1_SQRT_2),
@@ -375,19 +457,28 @@ fn player_keyboard_commands(
         _ => return,
     };
 
+    // Calculate new location, with speed adjusted by `time.delta_secs()` so that
+    // it's framerate independent.
     let new_location = location.get() + dir * time.delta_secs() * PLAYER_MOVE_SPEED;
 
+    // Very crude collision detection.
+    //
+    // We create two sensors:
+    // - One in the center of the line that makes up the left collision bound
+    // - Another to the right.
     let rect = global_bounds.bounds();
     let half_size = rect.half_size();
     let half_width = half_size.x;
     let half_height = half_size.y;
 
+    // If there is no grid value at the sample point, then it's passable.
     let sensor1 = Vec2::new(-half_width, half_height);
     let sensor1 = grid_query.grid_value_at(new_location + sensor1).is_none();
 
     let sensor2 = Vec2::new(half_width, half_height);
     let sensor2 = grid_query.grid_value_at(new_location + sensor2).is_none();
 
+    // If both sensors are clear, go ahead and move the skeleton.
     if sensor1 && sensor2 {
         location.set(new_location);
     }
@@ -458,8 +549,8 @@ fn main() {
         Update,
         (
             cloud_material_update_time,
-            camera_follow_skeleton,
-            camera_zoom_commands,
+            camera_and_clouds_follow_skeleton,
+            camera_mouse_wheel_zoom,
             track_current_level,
             player_keyboard_commands,
         )
